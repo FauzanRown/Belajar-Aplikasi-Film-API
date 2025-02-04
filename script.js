faaa9f1a5a9766bade2b2dd5b7524a06c01ce878
Muhammad Fauzan Anwar <zans.anwar@gmail.com>
2025-02-04 11:05:53 +0700

    Nambah Modal Dalam card ketika di klik

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 $.ajax({
-    url : 'http://www.omdbapi.com/?apikey=c1b3655d&s=CONJURING',
-    success : (data) => {
-        console.log(data)
+    url: 'http://www.omdbapi.com/?apikey=c1b3655d&s=avengers',
+    success: (data) => {
+        let card = '';
+        const movies = data.Search;
+
+        movies.forEach((DataMovie) => {
+            card += `
+            <div class="col-md-6 col-sm-12 col-lg-4 d-flex justify-content-center p-4">
+                <div class="card h-100 w-100">
+                    <img src="${DataMovie.Poster}" class="card-img-top" alt="${DataMovie.Title}">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">${DataMovie.Title}</h5>
+                        <p class="card-text">Tahun: ${DataMovie.Year}</p>
+                        <button class="btn btn-primary mt-auto show-modal" 
+                                data-bs-toggle="modal" 
+                                data-bs-target="#MovieDetailModal"
+                                data-title="${DataMovie.Title}"
+                                data-year="${DataMovie.Year}"
+                                data-poster="${DataMovie.Poster}">
+                            Selengkapnya
+                        </button>
+                    </div>
+                </div>
+            </div>`;
+        });
+
+        $(".movies_container").html(card);
+        console.log(movies);
     },
-    error : (gagal) => {
+    error: (gagal) => {
         console.log(gagal.responseText);
     }
-})
+});
